b33509fd058acf31208eea0f941b5a88c1a022dc
breadrock <breadrock1@gmail.com>
2025-01-07 23:33:45 +0300

    fix(test): fix test action

--- get-version.sh
+++ get-version.sh
@@ -3,6 +3,4 @@
 MANIFEST_PATH=$1
 
 cargo generate-lockfile --manifest-path $MANIFEST_PATH
-RESULT=$(cargo pkgid --manifest-path $MANIFEST_PATH | cut -d# -f2 | cut -d: -f2)
-
-echo $RESULT
+cargo pkgid --manifest-path $MANIFEST_PATH | cut -d# -f2 | cut -d: -f2
