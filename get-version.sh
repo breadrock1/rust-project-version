3d04e31cbd110c6b3b7bda301aa5b6842b7cd522
breadrock <breadrock1@gmail.com>
2025-01-08 00:12:19 +0300

    fix(test): fix test action

--- get-version.sh
+++ get-version.sh
@@ -5,4 +5,5 @@ MANIFEST_PATH=$1
 cargo generate-lockfile --manifest-path $MANIFEST_PATH
 OUTPUT=$(cargo pkgid --manifest-path $MANIFEST_PATH | cut -d# -f2 | cut -d: -f2)
 
+echo "project-version: $OUTPUT"
 echo "project-version=$OUTPUT" >> $GITHUB_OUTPUT
